0858449110e34cee5bad233a66d3d7f3b6f1b2c0
Giovanni <it@graficanappa.com>
2026-03-23 14:45:05 +0100

    Rimuovi script fix temporaneo

--- fix_66624.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-$u = App\Models\Ordine::where('commessa', '0066624')->update(['commessa' => '0066624-26']);
-echo "Ordini aggiornati: {$u}" . PHP_EOL;
