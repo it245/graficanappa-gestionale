05a81fd4a263963a8e7bf2e040f5398e8fc5b53d
Giovanni <it@graficanappa.com>
2026-05-18 08:49:05 +0200

    Check 67073: estendi a tutti PRD + ATTDocRighe per trovare piegaincolla mancante

--- check_fasi_67073.php
+++ check_fasi_67073.php
@@ -7,19 +7,47 @@ use Illuminate\Support\Facades\DB;
 
 $cod = '0067073-26';
 
-echo "=== ONDA: PRDDocFasi per $cod ===\n";
+echo "=== ONDA: ATTDocTeste (tutti TipoDocumento) per $cod ===\n";
+$att = DB::connection('onda')->select("
+    SELECT IdDoc, TipoDocumento, CodCommessa, DataRegistrazione
+    FROM ATTDocTeste WHERE CodCommessa = ?
+", [$cod]);
+foreach ($att as $r) {
+    echo "  AttIdDoc={$r->IdDoc} | TipoDoc={$r->TipoDocumento} | Data={$r->DataRegistrazione}\n";
+}
+
+echo "\n=== ONDA: PRDDocTeste (tutti PRD) per $cod ===\n";
+$prd = DB::connection('onda')->select("
+    SELECT IdDoc, CodCommessa, CodArt, OC_Descrizione
+    FROM PRDDocTeste WHERE CodCommessa = ?
+", [$cod]);
+foreach ($prd as $r) {
+    echo "  PRD={$r->IdDoc} | CodArt={$r->CodArt} | Desc=" . substr($r->OC_Descrizione, 0, 60) . "\n";
+}
+
+echo "\n=== ONDA: PRDDocFasi (tutte fasi da ogni PRD) per $cod ===\n";
 $onda = DB::connection('onda')->select("
     SELECT f.CodFase, f.CodMacchina, f.QtaDaLavorare, f.CodUnMis, f.TipoRiga, p.IdDoc
     FROM PRDDocTeste p
-    INNER JOIN ATTDocTeste t ON t.CodCommessa = p.CodCommessa
     LEFT JOIN PRDDocFasi f ON p.IdDoc = f.IdDoc
-    WHERE t.TipoDocumento = '2' AND t.CodCommessa = ?
-    ORDER BY f.CodFase
+    WHERE p.CodCommessa = ?
+    ORDER BY p.IdDoc, f.CodFase
 ", [$cod]);
 foreach ($onda as $r) {
     echo "  PRD={$r->IdDoc} | CodFase={$r->CodFase} | Macchina={$r->CodMacchina} | Qta={$r->QtaDaLavorare}{$r->CodUnMis} | TipoRiga={$r->TipoRiga}\n";
 }
-echo "Tot Onda: " . count($onda) . "\n";
+echo "Tot Onda fasi: " . count($onda) . "\n";
+
+echo "\n=== ONDA: ATTDocRighe (righe fattura) per $cod ===\n";
+$rig = DB::connection('onda')->select("
+    SELECT r.CodArt, r.Descrizione, r.TipoRiga, r.NrRiga
+    FROM ATTDocTeste t INNER JOIN ATTDocRighe r ON r.IdDoc = t.IdDoc
+    WHERE t.CodCommessa = ?
+    ORDER BY r.NrRiga
+", [$cod]);
+foreach ($rig as $r) {
+    echo "  Riga {$r->NrRiga} | TipoRiga={$r->TipoRiga} | CodArt={$r->CodArt} | Desc=" . substr($r->Descrizione ?? '', 0, 60) . "\n";
+}
 
 echo "\n=== MES: ordine_fasi per $cod ===\n";
 $ordini = DB::table('ordini')->where('commessa', $cod)->get();
